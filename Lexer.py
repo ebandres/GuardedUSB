430199742b2c238907ba0d51eaa9c03624bb4d33
Emmanuel Bandres <35436932+ebandres@users.noreply.github.com>
2019-10-01 20:28:40 -0400

    Some tokens

--- Lexer.py
+++ Lexer.py
@@ -2,6 +2,17 @@
 
 import ply
 
+# Tokens
+t_TkOBlock = r'\|\['
+t_TkCBlock = r'\]\|'
+t_TkSoForth = r'\.\.'
+t_TkComma = r','
+t_TkOpenPar = r'\('
+t_TkClosePar = r'\)'
+t_TkAsig = r':='
+t_TkSemiColon = r';'
+t_TkArrow = r'-->'
+
 class Lexer(object):
 	def __init__(self, source):
 		self.source = source
