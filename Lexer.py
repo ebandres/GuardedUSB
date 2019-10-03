c5a3fe69f41b8fa465381a8a0f9aa41d913f1a77
Daniela <daniela@mail.example.com>
2019-10-02 20:14:25 -0400

    mas tokens y palabras reservadas

--- Lexer.py
+++ Lexer.py
@@ -1,8 +1,25 @@
 # NOT FINISHED
 
-import ply
+import ply.lex as lex
+
+reserved = {
+        'declare' : 'TkDeclare',
+        'if' : 'TkIf',
+        'do' : 'TkDo',
+        'od' : 'TkOd',
+        'while' : 'TkWhile',
+        'for' : 'TkFor',
+        'else' : 'TkElse'
+        }
+
+tokens = ['TkOBlock','TkCBlock','TkSoForth','TkComma','TkOpenPar','TkClosePar','TkAsig',
+          'TkSemicolon','TkArrow','TkGuard','TkPlus','TkMinus','TkMult','TkDiv','TkMod',
+          'TkOr','TkAnd','TkNot','TkLess','TkLeq','TkGeq','TkGreater','TkEqual','TkNEqual',
+          'TkOBracket','TkCBracket','TkTwoPoints','TkConcat','TkAtoi','TkSize','TkMax',
+          'TkMin','TkId','TkNum','TkString'] + list(reserved.values())
 
 # Tokens
+t_ignore = ' \t\n#'
 t_TkOBlock = r'\|\['
 t_TkCBlock = r'\]\|'
 t_TkSoForth = r'\.\.'
@@ -12,6 +29,36 @@ t_TkClosePar = r'\)'
 t_TkAsig = r':='
 t_TkSemiColon = r';'
 t_TkArrow = r'-->'
+t_TkGuard = r'\[\]'
+t_TkPlus = r'\+'
+t_TkMinus = r'-'
+t_TkMult = r'\*'
+t_TkDiv = r'/'
+t_TkMod = r'%'
+t_TkOr = r'\\/'
+t_TkAnd = r'/\\'
+t_TkNot = r'!'
+t_TkLess = r'<'
+t_TkLeq = r'<='
+t_TkGeq = r'>='
+t_TkGreater = r'>'
+t_TkEqual = r'=='
+t_TkNEqual = r'!='
+t_TkOBracket = r'\['
+t_TkCBracket = r'\]'
+t_TkTwoPoints = r':'
+t_TkConcat = r'\|\|'
+t_TkString = r'^".*"$'
+
+def t_TkId(t):
+     r'[a-zA-Z_][a-zA-Z_0-9]*'
+     t.type = reserved.get(t.value,'TkId')    
+     return t
+
+def t_TkNum(t):
+     r'\d+'
+     t.value = int(t.value)
+     return t
 
 class Lexer(object):
 	def __init__(self, source):
